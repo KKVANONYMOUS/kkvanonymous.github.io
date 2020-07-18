77d149bf2a9e2d1296bd6d884706056f37adc0d9
Kunal Kumar Verma <ninja.kunal01@gmail.com>
2020-07-18 11:15:59 +0530

    Added div animation

--- main.js
+++ main.js
@@ -81,6 +81,39 @@ $(document).ready(function($) {
     $('#status').fadeOut(); // will first fade out the loading animation 
     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
     $('body').delay(350).css({ 'overflow': 'visible' });
+    AOS.init();
+
+    // You can also pass an optional settings object
+    // below listed default settings
+    AOS.init({
+        // Global settings:
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+    });
+
+    // ScrollReveal().reveal('.about_info',{ reset: true });
+    // ScrollReveal().reveal('.skills',{ reset: true });
+    // ScrollReveal().reveal('.arts',{ reset: true });
+    // ScrollReveal().reveal('.projects',{ reset: true });
+    // ScrollReveal().reveal('.contact',{ reset: true });
+    // ScrollReveal().reveal('.single_card',{ reset: true });
 });
 
 function topFunction() {
